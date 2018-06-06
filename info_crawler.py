b21400fbe3fc20867ddd6bc2486573e08ee2fd88
Rubi Mendes Passos <rubimpassos@gmail.com>
2018-06-06 13:08:02 -0300

    Real scenery

--- info_crawler.py
+++ info_crawler.py
@@ -1,9 +1,17 @@
 import json
+import sys
+
+import requests
 import xmltodict
+
 from bs4 import BeautifulSoup
+from xml.parsers.expat import ExpatError
 
 
 def parsed_description(description):
+    """Returns images, links and paragraphs of the description
+    :rtype: dict
+    """
     soup = BeautifulSoup(description, "html.parser")
     content = []
     for el in soup:
@@ -13,7 +21,7 @@ def parsed_description(description):
                     'type': "image",
                     'content': el.img['src']
                 })
-            # verifico se existe a organização div<ul<li<a
+            # check if div<ul<li<a exists
             elif el.ul and el.ul.li and el.ul.li.a:
                 links = []
                 for link in el.ul:
@@ -34,19 +42,47 @@ def parsed_description(description):
     return content
 
 
-with open('feed.xml', encoding='utf-8') as xml_feed:
-    xml_dict = xmltodict.parse(xml_feed.read())
-    item = xml_dict.get('item', {})
+def read_xml(url):
+    """Returns the xml data from url
+    :rtype: dict
+    """
+    reponse = requests.get(url)
+    xml_dict = {}
 
-output = {}
-for k, v in item.items():
-    if k in ['title', 'link']:
-        output[k] = v
-        continue
+    if reponse.status_code == 200:
+        try:
+            xml_dict = xmltodict.parse(reponse.content)
+        except ExpatError:
+            pass
 
-    if k == "description":
-        output['content'] = parsed_description(v)
+    return xml_dict
 
-feed_json = json.dumps(output)
-print(feed_json)
 
+def main(arg):
+    xml = read_xml("https://revistaautoesporte.globo.com/rss/ultimas/feed.xml")
+
+    feed = {'feed': []}
+    valid_fields = ['title', 'link', 'description']
+
+    #
+    if 'rss' in xml and 'channel' in xml.get('rss', {}):
+        channel = xml['rss'].get('channel', {})
+        items = channel.get('item', [])
+        for info in items:
+            # clean nonessential fields
+            invalid_fields = list(set(info.keys()) - set(valid_fields))
+            for field in invalid_fields:
+                info.pop(field, None)
+
+            if valid_fields[2] in info:
+                info['content'] = parsed_description(info[valid_fields[2]])
+                info.pop(valid_fields[2], None)
+
+            feed['feed'].append(info)
+
+    feed_json = json.dumps(feed, ensure_ascii=False, indent=True)
+    print(feed_json)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
