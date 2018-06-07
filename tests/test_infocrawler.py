284736e5f5feaf6856b8c08f22382e57a9588e3d
Rubi Mendes Passos <rubimpassos@gmail.com>
2018-06-07 20:46:11 -0300

    Separating html extract test context

--- tests/test_infocrawler.py
+++ tests/test_infocrawler.py
@@ -12,7 +12,7 @@ import info_crawler
 from bs4 import BeautifulSoup
 
 
-class RetrieveFeedTestCase(TestCase):
+class RetrieveFeedTest(TestCase):
     def _mock_response(self, status=200, content='CONTENT', json_data=None, raise_for_status=None):
         mock_resp = mock.Mock()
 
@@ -53,6 +53,45 @@ class RetrieveFeedTestCase(TestCase):
         session_instance.get.assert_called_with('some url')
 
 
+class ExtractHtmlTest(TestCase):
+    def test_extract_image_url(self):
+        expected_url = "http://www.exemple.org/image.jpg"
+
+        html_content = '<img src="{}" alt="Exemple Image" />'.format(expected_url)
+        soup = BeautifulSoup(html_content, "html.parser")
+        result_url = info_crawler.extract_image_url(soup.img)
+
+        self.assertEqual(expected_url, result_url)
+
+    def test_extract_text_content(self):
+        expected_text = "Exemple of captured text"
+
+        html_content = '<p>{}</p>'.format(expected_text)
+        soup = BeautifulSoup(html_content, "html.parser")
+        result_text = info_crawler.extract_text_content(soup.p)
+
+        self.assertEqual(expected_text, result_text)
+
+    def test_extract_links(self):
+        expected_links = [
+            "http://www.exemple.org",
+            "http://www.google.com",
+            "http://www.facebook.com",
+        ]
+
+        html_content = """
+        <ul>
+            <li><a href="{}" alt="exemple" >exemple</a></li>
+            <li><a href="{}" alt="google" >google</a></li>
+            <li><a href="{}" alt="facebook" >facebook</a></li>
+        </ul>
+        """.format(*expected_links)
+        soup = BeautifulSoup(html_content, "html.parser")
+        result_links = info_crawler.extract_links(soup.ul)
+
+        self.assertListEqual(expected_links, result_links)
+
+
 class InfoCrawlerTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -67,46 +106,6 @@ class InfoCrawlerTest(TestCase):
         with open(cls.json_path, encoding='utf-8') as f:
             cls.d_json = json.load(f)
             cls.feed_items = cls.d_json.get('feed')
-
-    def test_retrieve_feed_content(self):
-        rss_url = "https://revistaautoesporte.globo.com/rss/ultimas/feed.xml"
-        content = info_crawler.retrieve_feed(rss_url)
-
-        self.assertIsInstance(content, bytes)
-
-    def test_extract_image_url(self):
-        image_url = "http://www.exemple.org/image.jpg"
-        html = '<img src="{}" alt="Exemple Image" />'.format(image_url)
-        soup = BeautifulSoup(html, "html.parser")
-        extracted_url = info_crawler.extract_image_url(soup.img)
-
-        self.assertEqual(extracted_url, image_url)
-
-    def test_extract_text_content(self):
-        text = "Exemple of captured text"
-        html = '<p>{}</p>'.format(text)
-        soup = BeautifulSoup(html, "html.parser")
-        extracted_text = info_crawler.extract_text_content(soup.p)
-
-        self.assertEqual(extracted_text, text)
-
-    def test_extract_links(self):
-        links = [
-            "http://www.exemple.org",
-            "http://www.google.com",
-            "http://www.facebook.com",
-        ]
-        html = """
-        <ul>
-            <li><a href="{}" alt="exemple" >exemple</a></li>
-            <li><a href="{}" alt="google" >google</a></li>
-            <li><a href="{}" alt="facebook" >facebook</a></li>
-        </ul>
-        """.format(*links)
-        soup = BeautifulSoup(html, "html.parser")
-        extracted_links = info_crawler.extract_links(soup.ul)
-
-        self.assertListEqual(extracted_links, links)
 
     def test_parsed_description(self):
         j_content = self.feed_items[0].get('description')
