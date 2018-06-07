cc53eeec1b33e5668c8d264a29b29701a6d2e0f5
Rubi Mendes Passos <rubimpassos@gmail.com>
2018-06-07 16:51:05 -0300

    Adding first tests

--- tests.py
+++
@@ -1,5 +0,0 @@
-from unittest import TestCase
-
-class InfoCrawlerTest(TestCase):
-    def test_read_xml(self):
-        pass
---
+++ tests/test_infocrawler.py
@@ -0,0 +1,62 @@
+import json
+import os
+from unittest import TestCase
+
+from bs4 import BeautifulSoup
+
+from info_crawler import extract_image_url, extract_text_content, extract_links, parsed_description, parse_item
+
+
+class InfoCrawlerTest(TestCase):
+    def setUp(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.xml_path = os.path.join(dir_path, "feed.xml")
+        self.json_path = os.path.join(dir_path, "feed.json")
+
+        with open(self.xml_path, encoding='utf-8') as f:
+            self.xml_soup = BeautifulSoup(f, "xml")
+
+        with open(self.json_path, encoding='utf-8') as f:
+            self.d_json = json.load(f)
+
+    def test_extract_image_url(self):
+        image_url = "http://www.exemple.org/image.jpg"
+        html = '<img src="{}" alt="Exemple Image" />'.format(image_url)
+        soup = BeautifulSoup(html, "html.parser")
+        extracted_url = extract_image_url(soup.img)
+
+        self.assertEqual(extracted_url, image_url)
+
+    def test_extract_text_content(self):
+        text = "Exemple of captured text"
+        html = '<p>{}</p>'.format(text)
+        soup = BeautifulSoup(html, "html.parser")
+        extracted_text = extract_text_content(soup.p)
+
+        self.assertEqual(extracted_text, text)
+
+    def test_extract_links(self):
+        links = [
+            "http://www.exemple.org",
+            "http://www.google.com",
+            "http://www.facebook.com",
+        ]
+        html = """
+        <ul>
+            <li><a href="{}" alt="exemple" >exemple</a></li>
+            <li><a href="{}" alt="google" >google</a></li>
+            <li><a href="{}" alt="facebook" >facebook</a></li>
+        </ul>
+        """.format(*links)
+        soup = BeautifulSoup(html, "html.parser")
+        extracted_links = extract_links(soup.ul)
+
+        self.assertListEqual(extracted_links, links)
+
+    def test_parsed_description(self):
+        feed = self.d_json.get('feed')
+        j_content = feed[0].get('content')
+        description = self.xml_soup.find('item').find('description').text
+        extracted_content = parsed_description(description)
+
+        self.assertEqual(str(extracted_content), str(j_content))
